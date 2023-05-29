484253cb8f0bd40562fa6dde9b700bb868216be8
Yale Araujo <yale850@gmail.com>
2023-05-29 13:41:53 -0300

    style: importing global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "styled-components"
 import { defaultTheme } from "./styles/themes/default"
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <h1>Jesus Cristo Obrigado</h1>
+      <GlobalStyle />
     </ThemeProvider>
 
   )

e7e9962e4ed62f3b0bfaae07dbe9ffa65e075f39
Yale Araujo <yale850@gmail.com>
2023-05-31 10:19:50 -0300

    feat: added all html from home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,32 @@
+import { Play } from 'phosphor-react'
+
 export function Home() {
-  return <h1>God is wonderful!</h1>
+  return (
+    <div>
+      <form action="">
+        <div>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <input id="minutesAmount" type="number" />
+
+          <span>minutos.</span>
+        </div>
+
+        <div>
+          <span>0</span>
+          <span>0</span>
+          <span>:</span>
+          <span>0</span>
+          <span>0</span>
+        </div>
+
+        <button type="submit">
+          <Play />
+          Começar
+        </button>
+      </form>
+    </div>
+  )
 }

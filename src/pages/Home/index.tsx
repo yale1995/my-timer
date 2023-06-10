ca22b5a50e72ded3ef6e5cbc6882ec6f53d42c60
Yale Araujo <yale850@gmail.com>
2023-06-10 19:59:43 -0300

    feat: new variable to catch data from active cycle

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -55,6 +55,7 @@ export function Home() {
     reset()
   }
 
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
   const task = watch('task')
   const isSubmitDisabled = !task
 

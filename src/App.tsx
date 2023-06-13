7e713c8eae6c4abff855d6a3d739d17208058080
Yale Araujo <yale850@gmail.com>
2023-06-12 23:32:43 -0300

    feat: adding context provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
+import { CyclesContextProvider } from './context/CyclesContext'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
-        <Router />
-        <GlobalStyle />
-      </BrowserRouter>
+      <CyclesContextProvider>
+        <BrowserRouter>
+          <Router />
+          <GlobalStyle />
+        </BrowserRouter>
+      </CyclesContextProvider>
     </ThemeProvider>
   )
 }

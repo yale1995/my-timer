a4582dcd2b3da139750378b536ff85d76a0447c5
Yale Araujo <yale850@gmail.com>
2023-06-19 11:56:50 -0300

    fix: add new cycle

--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -19,7 +19,8 @@ export function cyclesReducer(state: CyclesState, action: any) {
   switch (action.type) {
     case ActionTypes.ADD_NEW_CYCLE:
       return produce(state, (draft) => {
-        draft.cycles.push(action.payload.newCycle.id)
+        draft.cycles.push(action.payload.newCycle)
+        draft.activeCycleId = action.payload.newCycle.id
       })
 
     case ActionTypes.INTERRUPT_CURRENT_CYCLE: {
